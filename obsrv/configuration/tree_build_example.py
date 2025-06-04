fd28c3feb1a9f6dba613aeddad470a4df332187a
majkelx <mkalusz@camk.edu.pl>
2025-06-04 15:48:18 +0200

    dev config and deps cleanup

--- obsrv/configuration/tree_build_example.py
+++ obsrv/configuration/tree_build_example.py
@@ -6,55 +6,72 @@ from obsrv.data_colection.base_components.tree_provider import TreeProvider
 from obsrv.data_colection.specialistic_components.tree_alpaca import TreeAlpacaObservatory
 from obsrv.data_colection.specialistic_components.tree_blocker_access_grantor import TreeBlockerAccessGrantor
 from obsrv.data_colection.specialistic_components.tree_cache_observatory import TreeCache
-from obsrv.data_colection.specialistic_components.tree_cctv import TreeCCTV
+# from obsrv.data_colection.specialistic_components.tree_cctv import TreeCCTV
 from obsrv.data_colection.specialistic_components.tree_conditional_freezer import TreeConditionalFreezer
 from obsrv.data_colection.specialistic_components.tree_base_request_blocker import TreeBaseRequestBlocker
+from obsrv.data_colection.specialistic_components.tree_custom_guider_handler import TreeCustomGuiderHandler
 from obsrv.data_colection.specialistic_components.tree_ephemeris import TreeEphemeris
 from obsrv.data_colection.specialistic_components.tree_plan_executor import TreePlanExecutor
 from obsrv.ob_config import SingletonConfig
 
 
 def tree_build() -> Router:
-    # load custom config file
+    """This is the Example tree structure, suitable for development. It defines dev telescope.
+
+    This file is loaded by the default configuration file obsrv/config.yaml
+    The corresponding components tree is loaded from obsrv/configuration/sample_config.yaml
+
+    In the production environment, configuration should be defined in
+        obsrv/configuration/config.yaml or /usr/local/etc/ocabox/config.yaml,
+    and the tree should be build be corresponding custom tree_build_xxxx.py
+    """
+
     SingletonConfig.add_config_file_from_config_dir('sample_config.yaml')
     SingletonConfig.get_config(rebuild=True).get()  # this method refreshes the configuration with newly added files,
-    # it must be called once after adding all files
-    # tree
+
     # --------------------------------------- global ---------------------------------------
     ephemeris = TreeEphemeris('ephemeris', 'ephemeris')
     broker_components_global = TreeBaseBrokerDefaultTarget('broker-components-global',
-                                                           [ephemeris],
-                                                           default_provider=None)
+                                                         [ephemeris],
+                                                         default_provider=None)
     cache_global = TreeCache('cache-global', broker_components_global)
     conditional_freezer_global = TreeConditionalFreezer('conditional-freezer-global', cache_global)
     target_provider_global = TreeProvider('target-provider-global', 'global', conditional_freezer_global)
 
-    # --------------------------------------- zb08 ---------------------------------------
-    alpaca_zb08 = TreeAlpacaObservatory('alpaca-zb08', observatory_name='zibi-ocabox-800')
-    alpaca_blocker_zb08 = TreeBaseRequestBlocker('alpaca-blocker-zb08', alpaca_zb08)
-    blocker_grantor_zb08 = TreeBlockerAccessGrantor('access-grantor-zb08', 'access_grantor', alpaca_blocker_zb08)
-    plan_executor_zb08 = TreePlanExecutor('executor-zb08', 'executor')
-    cctv_zb08 = TreeCCTV('cctv-zb08', 'cctv')
-    # ephemeris_zb08 = TreeEphemeris('ephemeris-zb08', 'ephemeris')
-    broker_components_zb08 = TreeBaseBrokerDefaultTarget('broker-components-zb08',
-                                                         [blocker_grantor_zb08, plan_executor_zb08, cctv_zb08],
-                                                         default_provider=alpaca_blocker_zb08)
-    cache_zb08 = TreeCache('cache-zb08', broker_components_zb08)
-    conditional_freezer_zb08 = TreeConditionalFreezer('conditional-freezer-zb08', cache_zb08)
-    target_provider_zb08 = TreeProvider('target-provider-zb08', 'zb08', conditional_freezer_zb08)
+    # --------------------------------------- sim ---------------------------------------
+    alpaca_sim = TreeAlpacaObservatory('alpaca-sim', observatory_name='sim')
+    alpaca_blocker_sim = TreeBaseRequestBlocker('alpaca-blocker-sim', alpaca_sim)
+    blocker_grantor_sim = TreeBlockerAccessGrantor('access-grantor-sim', 'access_grantor', alpaca_blocker_sim)
+    plan_executor_sim = TreePlanExecutor('executor-sim', 'executor')
+    broker_components_sim = TreeBaseBrokerDefaultTarget('broker-components-sim',
+                                                        [blocker_grantor_sim, plan_executor_sim],
+                                                        default_provider=alpaca_blocker_sim)
+    cache_sim = TreeCache('cache-sim', broker_components_sim)
+    conditional_freezer_sim = TreeConditionalFreezer('conditional-freezer-sim', cache_sim)
+    target_provider_sim = TreeProvider('target-provider-sim', 'sim', conditional_freezer_sim)
 
-    # ---------
-    alpaca_module = TreeAlpacaObservatory('zibi_sample', observatory_name='zibi-ocabox-800')
-    alpaca_blocker = TreeBaseRequestBlocker('zibi_blocker', alpaca_module)
-    blocker_grantor = TreeBlockerAccessGrantor('grantor_800', 'access_grantor', alpaca_blocker)
-    broker_800 = TreeBaseBrokerDefaultTarget('broker_800', [blocker_grantor], default_provider=alpaca_blocker)
-    cache_800 = TreeCache('sample_cache', broker_800)
-    conditional_freezer_800 = TreeConditionalFreezer('conditional_freezer_800', cache_800)
+    # --------------------------------------- dev ---------------------------------------
+    alpaca_dev = TreeAlpacaObservatory('alpaca-dev', observatory_name='dev')
+    alpaca_blocker_dev = TreeBaseRequestBlocker('alpaca-blocker-dev', alpaca_dev)
+    blocker_grantor_dev = TreeBlockerAccessGrantor('access-grantor-dev', 'access_grantor', alpaca_blocker_dev)
+    plan_executor_dev = TreePlanExecutor('executor-dev', 'executor')
+    custom_guider_handler = TreeCustomGuiderHandler('custom-guider-handler', 'custom_guider', alpaca_dev)
+    broker_components_dev = TreeBaseBrokerDefaultTarget('broker-components-dev',
+                                                        [blocker_grantor_dev, plan_executor_dev, custom_guider_handler],
+                                                        default_provider=alpaca_blocker_dev)
+    cache_dev = TreeCache('cache-dev', broker_components_dev)
+    conditional_freezer_dev = TreeConditionalFreezer('conditional-freezer-dev', cache_dev)
+    target_provider_dev = TreeProvider('target-provider-dev', 'dev', conditional_freezer_dev)
 
-    tp_800 = TreeProvider('module_800', '800', conditional_freezer_800)
-    vb = TreeBaseBroker('front_broker', [tp_800, target_provider_global, target_provider_zb08])
 
-    # front receiver blocks
-    rs = RequestSolver(vb)
-    vr = Router(rs)
+    # -----------------------------gather alpacas components -----------------------------
+    broker_front_oca = TreeBaseBroker('broker-front-oca',[
+                                       target_provider_sim,
+                                       target_provider_dev,
+                                       target_provider_global
+    ])
+
+    # ------------------------------ front receiver blocks -------------------------------
+    rs = RequestSolver(data_provider=broker_front_oca)
+    vr = Router(request_solver=rs, name="Router-OCA")
     return vr
